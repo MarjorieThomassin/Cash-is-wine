74f905ad8698e40c4b83503074e22759563203d2
MarjorieThomassin <marjorie.thomassin5@gmail.com>
2022-01-24 15:14:10 +0100

    created Login and Home controllers and views, added some style

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+class LoginController extends AbstractController {
+    public function login()
+    {
+        return $this->twig->render('Login/login.html.twig');
+    }
+}
